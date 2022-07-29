c11c317fd857881dccea9a5ec4ec20d6fcf7dde5
wittlerfilho <wittlerfilho@gmail.com>
2022-07-29 19:30:23 -0300

    first commit

---
+++ dist/index.6332a54f.js
@@ -0,0 +1,2 @@
+
+//# sourceMappingURL=index.6332a54f.js.map
---
+++ index.js
@@ -0,0 +1,9 @@
+import { annotate, annotationGroup } from 'rough-notation';
+
+const e = document.querySelector('.main-span');
+const annotation = annotate(e, { type: 'highlight', strokeWidth: 10, color: '#DADADA', animationDuration: 1500 });
+annotation.show();
+
+window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+  }
