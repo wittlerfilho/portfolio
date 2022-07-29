e9f8d6b0fcad3e650e5a91cc869b1b72f2851565
wittlerfilho <wittlerfilho@gmail.com>
2022-07-29 20:10:02 -0300

    rough-notation fix

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import { annotate, annotationGroup } from 'rough-notation';
+// import { annotate, annotationGroup } from 'rough-notation';
 
-const e = document.querySelector('.main-span');
-const annotation = annotate(e, { type: 'highlight', strokeWidth: 10, color: '#DADADA', animationDuration: 1500 });
-annotation.show();
+// const e = document.querySelector('.main-span');
+// const annotation = annotate(e, { type: 'highlight', strokeWidth: 10, color: '#DADADA', animationDuration: 1500 });
+// annotation.show();
 
-window.onbeforeunload = function () {
-    window.scrollTo(0, 0);
-  }
+// window.onbeforeunload = function () {
+//     window.scrollTo(0, 0);
+//   }
